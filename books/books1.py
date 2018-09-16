fde8e514c7b0e412da8ed5b163812e6556fbf9a7
HahnJustin <hahnj2@gmail.com>
2018-09-16 14:36:20 -0500

    Updated books1.py, so it is better

--- books/books1.py
+++ books/books1.py
@@ -1,11 +1,43 @@
 import csv
+import os
+import sys
+import re
 
 def scanner(name) :
+    booksData = []
     with open(name, newline='') as f:
         reader = csv.reader(f)
         for row in reader:
-            print(row)
+            booksData.append(row)
+    return booksData
 
 def main():
     csv = sys.argv[1]
-    scanner(csv)
+    booksData = scanner(csv)
+    #print(booksData)
+    books = []
+    authors = []
+    authorsTemp = []
+    authorsTempData = []
+    authorsFinal = []
+    
+    for bookList in booksData:
+        books.append(bookList[0])
+        authors.append(bookList[2]) 
+    
+    for author in authors:
+        authorsTemp = re.split(' \(|\) and ',author)
+        authorsTemp.pop()
+        if len(authorsTemp) > 1:
+            del authorsTemp[1]
+            # does not account for three authors
+        authorsTempData.append(authorsTemp)
+        # Need to compare lists of two authors
+
+    print(authorsTempData)
+    
+    #print("BOOKS ")
+    #print(books)
+    #print("AUTHORS ")
+    #print(authors)
+main()
