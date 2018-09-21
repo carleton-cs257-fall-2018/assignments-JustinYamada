6dbfa9bc1503a1be29415da2fd8e3a2eca820d1c
JustinYamada <yamadaj@carleton.edu>
2018-09-21 16:40:46 -0500

    adding the new tests for the other methods

--- books2/booksdatasourcetest2.py
+++ books2/booksdatasourcetest2.py
@@ -14,7 +14,7 @@ class booksDataSourceTest(unittest.TestCase):
         pass
 
     def test_book(self):
-        self.assertEqual(booksdatasource.book(5), "Book Five")
+        self.assertEqual(booksdatasource.book(5), "5, Book Five, 1005")
 
     def test_books(self):
         self.assertEqual(booksdatasource.book(3), "Book Three")
@@ -23,13 +23,25 @@ class booksDataSourceTest(unittest.TestCase):
         self.assertEqual(booksdatasource.author(3), "Charlie Author Three")
 
     def test_authors(self)
-        self.assertEqual(booksdatasource.authors(), [Alpha Author One, Bravo Author Two, Charlie Author Three, Delta Author Four, Echo Author Five])
+        self.assertEqual(booksdatasource.authors(),
+        [Alpha Author One, Bravo Author Two, Charlie Author Three, Delta Author Four, Echo Author Five])
 
-    def test_composite(self):
-        self.assertFalse(self.prime_checker.is_prime(96))
+    def test_books_for_author_for_single_book(self):
+        self.assertEqual(booksdatasource.books_for_author(1),
+        {'id': 1, 'title': Book One, 'publication year': 1001})
 
-    def test_primes_below(self):
-        self.assertEqual(self.prime_checker.get_primes_below(20), [2, 3, 5, 7, 11, 13, 17, 19])
+    def test_books_for_author_for_single_book(self):
+        self.assertEqual(booksdatasource.books_for_author(),
+        {'id': 1, 'title': Book One, 'publication year': 1001}, {'id': 1, 'title': Book One, 'publication year': 1001})
+
+    def test_authors_for_book_for_single_author(self):
+        self.assertEqual(booksdatasource.authors_for_book(4),
+        {'id': 4, 'last_name': Four, 'first_name': Echo, 'birth_year': 250, 'death_year': 1004})
+
+    def test_authors_for_book_for_multiple_author(self):
+        self.assertEqual(booksdatasource.authors_for_book(4),
+        {'id': 4, 'last_name': Four, 'first_name': Echo, 'birth_year': 250, 'death_year': 1004},
+        {'id': 4, 'last_name': Four, 'first_name': Echo, 'birth_year': 250, 'death_year': 1004})
 
     def test_wrong_author_id(self):
         self.assertRaises(ValueError, self.data_source.books,author_id=-1)
