6053e7f78309e5dbb7d05b05bfcf4e8dc300329f
JustinYamada <yamadaj@carleton.edu>
2018-09-21 16:53:38 -0500

    Adding new methods and the changing of the csv file

--- books2/booksdatasourcetest2.py
+++ books2/booksdatasourcetest2.py
@@ -8,29 +8,35 @@ import unittest
 
 class booksDataSourceTest(unittest.TestCase):
     def setUp(self):
-        self.booksDataSourceTest = booksdatasource.BooksDataSource('books.csv','authors.csv', 'books_authors_link.csv')
+        self.booksDataSourceTest = booksdatasource.BooksDataSource('bookstest.csv','authorstest.csv', 'books_authorstest.csv')
 
     def tearDown(self):
         pass
 
     def test_book(self):
-        self.assertEqual(booksdatasource.book(5), "5, Book Five, 1005")
+        self.assertEqual(booksdatasource.book(5), {'id': 5, 'title': 'Book Five', 'publication_year': 1005})
 
-    def test_books(self):
-        self.assertEqual(booksdatasource.book(3), "Book Three")
+    def test_books_only_id(self):
+        self.assertEqual(booksdatasource.book(3), 'id': 3, 'title': 'Book Three', 'publication_year': 1003})
+
+    def test_books_with_start_end_year(self):
+        self.assertEqual(booksdatasource.book(3), 'id': 3, 'title': 'Book Three', 'publication_year': 1003})
 
     def test_author(self):
-        self.assertEqual(booksdatasource.author(3), "Charlie Author Three")
+        self.assertEqual(booksdatasource.author(3), {'id': 3, 'last_name': "Three", 'first_name': "Delta", 'birth_year': 200, 'death_year': 1003})
 
     def test_authors(self)
         self.assertEqual(booksdatasource.authors(),
         [Alpha Author One, Bravo Author Two, Charlie Author Three, Delta Author Four, Echo Author Five])
 
+    def test_authors(self)
+        self.assertEqual(booksdatasource.authors(), [Alpha Author One, Bravo Author Two, Charlie Author Three, Delta Author Four, Echo Author Five])
+
     def test_books_for_author_for_single_book(self):
         self.assertEqual(booksdatasource.books_for_author(1),
         {'id': 1, 'title': Book One, 'publication year': 1001})
 
-    def test_books_for_author_for_single_book(self):
+    def test_books_for_author_for_multiple_book(self):
         self.assertEqual(booksdatasource.books_for_author(),
         {'id': 1, 'title': Book One, 'publication year': 1001}, {'id': 1, 'title': Book One, 'publication year': 1001})
 
