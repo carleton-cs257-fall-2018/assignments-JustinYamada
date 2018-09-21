35a2f76441069e59d7fd13ab5a7974b9b3ccac3f
JustinYamada <yamadaj@carleton.edu>
2018-09-21 12:54:04 -0500

    created four new tests

--- books2/booksdatasourcetest.py
+++ books2/booksdatasourcetest.py
@@ -13,20 +13,24 @@ class booksDataSourceTest(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_zero(self):
-        self.assertRaises(ValueError, self.prime_checker.is_prime, 0)
+    def test_book(self):
+        self.assertEqual(booksdatasource.book(5), "Book Five")
 
-    def test_two(self):
-        self.assertTrue(self.prime_checker.is_prime(2))
+    def test_author(self):
+        self.assertEqual(booksdatasource.author(3), "Charlie Author Three")
 
-    def test_three(self):
-        self.assertTrue(self.prime_checker.is_prime(97))
+    def test_authors_return_all_authors(self)
+        self.assertEqual(booksdatasource.authors(), [Alpha Author One, Bravo Author Two, Charlie Author Three, Delta Author Four, Echo Author Five])
+
 
     def test_composite(self):
         self.assertFalse(self.prime_checker.is_prime(96))
 
     def test_primes_below(self):
         self.assertEqual(self.prime_checker.get_primes_below(20), [2, 3, 5, 7, 11, 13, 17, 19])
+
+    def test_wrong_author_id(self):
+        self.assertRaises(ValueError, self.data_source.books,author_id=-1)
 
 if __name__ == '__main__':
     unittest.main()
