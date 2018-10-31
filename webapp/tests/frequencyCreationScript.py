ff25d42189a5c59d84517c5f3e31403d75ace750
HahnJustin <hahnj2@gmail.com>
2018-10-30 22:02:43 -0500

    uploaded updated csv frequency creator and csv

--- webapp/tests/frequencyCreationScript.py
+++ webapp/tests/frequencyCreationScript.py
@@ -17,6 +17,7 @@ def load_from_crime_csv_file(csv_file_name):
     reader = csv.reader(csv_file)
 
     crime_frequency = {}
+    code_to_crime = {}
     FirstRowSkip = True
     for row in reader:
         if FirstRowSkip == True:
@@ -25,22 +26,24 @@ def load_from_crime_csv_file(csv_file_name):
         #assert len(row) == 26
 
         police_code = row[1]
+        specific_crime = row[8]
         if police_code not in crime_frequency.keys():
             crime_frequency[police_code] = 1
+            code_to_crime[police_code] = specific_crime
         else:
             crime_frequency[police_code] += 1
 
     csv_file.close()
-    return (crime_frequency)
+    return (code_to_crime, crime_frequency)
 
-def save_crime_frequency_table(crime_frequency, csv_file_name):
+def save_crime_frequency_table(code_to_crime, crime_frequency, csv_file_name):
     output_file = open(csv_file_name, 'w', encoding='utf-8')
     writer = csv.writer(output_file)
     for key in sorted(crime_frequency, key=crime_frequency.get, reverse = True):
-        row = [key, crime_frequency[key]]
+        row = [code_to_crime[key], key, crime_frequency[key]]
         writer.writerow(row)
     output_file.close()
 
 if __name__ == '__main__':
-    crime_frequency_dict = load_from_crime_csv_file('Crimetest.csv')
-    save_crime_frequency_table(crime_frequency_dict, 'crime_frequency.csv')
+    code_to_crime_dict, crime_frequency_dict = load_from_crime_csv_file('Crime.csv')
+    save_crime_frequency_table(code_to_crime_dict, crime_frequency_dict, 'crime_frequency.csv')
