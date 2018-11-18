33b594d218bad7d276930b1c15cc3f2753604fc1
Conor Gormally <ConorGormally@eduroam-163-19.dyn.carleton.edu>
2018-11-18 16:48:13 -0600

    finalizing the project and its names

---
+++ final/saveFlamieBoi/src/flamieBoi/Block.java
@@ -0,0 +1,22 @@
+/**
+ * Block.java
+ * @author Justin Yamada
+ * @author Conor Gormally
+ * @date November 18, 2018
+ *
+ * A subclass of rectangle for the sprites of the blocks.
+ */
+package flamieBoi;
+
+import javafx.scene.image.Image;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.paint.ImagePattern;
+
+public class Block extends Rectangle {
+
+    public Block() {
+        Image image = new Image("http://pixelartmaker.com/art/9a13872d51e52cf.png");
+        setFill(new ImagePattern(image));
+    }
+
+}
---
+++ final/saveFlamieBoi/src/flamieBoi/Main.java
@@ -0,0 +1,62 @@
+/**
+ * Main.java
+ * @author Justin Yamada
+ * @author Conor Gormally
+ * @date November 18, 2018
+ *
+ * The main program for a pong-like program in JavaFX. The goal
+ * of the game is to keep the bouncing flame within the screen
+ * using the 4 blocks of grass.
+ */
+
+package flamieBoi;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+public class Main extends Application {
+
+    /**
+     *
+     * @param primaryStage
+     * @throws Exception
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent t) {
+                Platform.exit();
+                System.exit(0);
+            }
+        });
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("flamieBoi.fxml"));
+        Parent root = (Parent)loader.load();
+        Controller controller = loader.getController();
+
+        //title of stage
+        primaryStage.setTitle("Keep Flamie Boi Alive");
+
+        //sets the scene to be 700 by 500 pixels
+        Scene scene = new Scene(root, 700, 500);
+        primaryStage.setScene(scene);
+
+        //if key stroke taken in send key stroke to controller
+        scene.setOnKeyPressed(controller);
+        primaryStage.show();
+
+        root.requestFocus();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
